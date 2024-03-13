1c1ad870ef69c12d4abdc7d9030b6a7419739441
mkotyak <kotyak.maria@gmail.com>
2024-03-13 15:44:16 +0300

    Initialized NavigationStackExperiment project

---
+++ NavigationStackExperiment/NavigationStackExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackExperimentApp.swift
+//  NavigationStackExperiment
+//
+//  Created by Maria Kotyak on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

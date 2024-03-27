a58a08a82a70567488e11b303b712aaecff34044
mkotyak <kotyak.maria@gmail.com>
2024-03-27 12:08:57 +0300

    Initialized NavigationStackMVVM project

---
+++ NavigationStackMVVM/NavigationStackMVVM/NavigationStackMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackMVVMApp.swift
+//  NavigationStackMVVM
+//
+//  Created by Maria Kotyak on 27/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

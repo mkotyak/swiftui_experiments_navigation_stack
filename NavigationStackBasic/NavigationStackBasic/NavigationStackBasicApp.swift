8cf7e9abd0d92717f662abd6482ada4c0bdd76ad
mkotyak <kotyak.maria@gmail.com>
2024-03-27 09:59:07 +0300

    Created NavigationStackBasic project

---
+++ NavigationStackBasic/NavigationStackBasic/NavigationStackBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackBasicApp.swift
+//  NavigationStackBasic
+//
+//  Created by Maria Kotyak on 27/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

40b160c6e28d12280c62ebb36074534c927adf99
mkotyak <kotyak.maria@gmail.com>
2024-03-29 11:07:33 +0300

    Initialized NavigationStackMVVMWithNavigationPath project

---
+++ NavigationStackMVVMWithNavigationPath/NavigationStackMVVMWithNavigationPath/NavigationStackMVVMWithNavigationPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackMVVMWithNavigationPathApp.swift
+//  NavigationStackMVVMWithNavigationPath
+//
+//  Created by Maria Kotyak on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackMVVMWithNavigationPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
